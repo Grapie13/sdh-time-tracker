cb164c278132a8158f7fe375fccb2e3ef8ab0e56
Grapie13 <kapper12345@gmail.com>
2021-08-07 16:46:15 +0200

    Fixed a typo in DTO comments

--- src/task/dto/updateTask.dto.ts
+++ src/task/dto/updateTask.dto.ts
@@ -1,4 +1,4 @@
-// Creating entity properties with a bang operator (!),
+// Creating DTO properties with a bang operator (!),
 // because strict checking is enabled in tsconfig.
 class UpdateTaskDto {
   id!: number;
